a7f0c31511edf4b4eee4866adced5a7f943b7b68
nmicaux <micaux.nicolas@gmail.com>
2022-10-19 19:44:58 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,24 @@ from distutils.core import setup
 setup(
     name='searchin',
     packages=['searchin'],
-    version='0.1.5',
+    version='0.1.7',
     description='Search anything in a live python object, method or variable!',
-    author='Nicolas Micaux',  # Type in your name
-    # author_email='your.email@domain.com',  # Type in your E-Mail
+    author='Nicolas Micaux',
+
     url='https://github.com/NicolasMICAUX/searchin',
     # download_url='https://github.com/NicolasMICAUX/searchin/archive/refs/tags/v0.1.5.tar.gz',
-    keywords=['SEARCH', 'DEBUG'],
+
     install_requires=[],  # None
+
+    keywords=['SEARCH', 'DEBUG'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Natural Language :: English',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Utilities',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -28,5 +33,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
+    python_requires='>=2.7',
 
+    # Set README.md as description
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
