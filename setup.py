dcad8660687d99325e35fc9565521b7606bab853
NicolasMICAUX <micaux.nicolas@gmail.com>
2023-02-09 11:51:29 +0100

    minor

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='searchin',
     packages=['searchin'],
-    version='0.2.1',
+    version='0.2.2',
     description='Search anything in a live python object, method or variable',
     author='Nicolas Micaux',
 
